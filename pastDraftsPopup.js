fc371abd79957e9d032d1dc1ee0ae4a49ca6f1d5
Brice Shatzer <bshatzer@kinja.com>
2018-01-02 09:24:49 -0600

    psudeo code?

--- pastDraftsPopup.js
+++ pastDraftsPopup.js
@@ -7,17 +7,42 @@ console.dir(this);
  window.onload = function() { 
  	console.log("window.onload ----");
  	console.log(document.getElementById('button'));
+
+
  } 
 
+
+function generatePopupWindow(htmlString){
+	var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top="+(screen.height-400)+",left="+(screen.width-840));
+	win.document.body.innerHTML = htmlString;
+}
+
+function getEditorState() {
+	return document.querySelector('.editor-inner').innerHtml
+}
+
+
 /*
-- on an interval - 
+- on an interval in background - 
+1. create a new save point consisting of:
+	- document.querySelector('.editor-inner').innerHtml;
+	- time stamp 
+2. if contents of the new save is different that the prior save,
+	Push that save point to local storage for the page. 
+3. If more then 5 saves, remove oldest. 
 
-create a new save point based on 
+
+- on open popup - 
+1. Grab all of the save points in local storage for the page.
+2. write control markup for each of them to the pop html 
+3. attach clickHandlers which link each control to it's corresponding data,
+	allowing it to (?push it's contents to the textarea?) / (?open it popup with the text?)
 
 
-//create new node for 
 
-
+//create popup with contents
+var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top="+(screen.height-400)+",left="+(screen.width-840));
+win.document.body.innerHTML = "HTML";
 
 
 
